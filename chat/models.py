401d5317765b7853be69a20df2c99072f758e2a0
TERA <p.tyche92@gmail.com>
2026-01-29 11:45:33 +0900

    [feat] 이전채팅 히스토리 저장

--- chat/models.py
+++ chat/models.py
@@ -1,12 +1,15 @@
 # chat/models.py
 from django.db import models
 
+
 class ChatMessage(models.Model):
     ROLE_CHOICES = (
         ("user", "User"),
         ("ai", "AI"),
     )
 
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=True)
+    session_id = models.CharField(max_length=100, db_index=True, null=True, blank=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
